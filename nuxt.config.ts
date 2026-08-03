748bc318f9ef52f73c878d320dd634906f6fe09e
notizen-00 <dipta.ikromi@unej.ac.id>
2026-08-04 00:58:31 +0700

    logo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,27 @@ export default defineNuxtConfig({
   ssr: false,
   modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss'],
   css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'id',
+      },
+      meta: [
+        { name: 'theme-color', content: '#e7194a' },
+        { name: 'application-name', content: 'Sewantara Mitra' },
+        { name: 'apple-mobile-web-app-title', content: 'Sewantara Mitra' },
+        { name: 'apple-mobile-web-app-capable', content: 'yes' },
+        { name: 'apple-mobile-web-app-status-bar-style', content: 'default' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon_io/favicon.ico' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon_io/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon_io/favicon-16x16.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon_io/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/favicon_io/site.webmanifest' },
+      ],
+    },
+  },
   runtimeConfig: {
     public: {
       apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost',
